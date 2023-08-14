60b304c2a2b9d901dea88105c9e04c0e83747960
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-14 18:05:20 -0400

    edit task functionality, yes

--- src/App.js
+++ src/App.js
@@ -5,29 +5,38 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
-    const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-    const addTask = (task) => {
-        const newTask = {
-            id: uuidv4(), // Generates a unique ID
-            content: task,
-        };
-        console.log(newTask);
-        setTasks([...tasks, newTask]);
-    }
+  const addTask = (task) => {
+      const newTask = {
+          id: uuidv4(),
+          content: task,
+      };
+      setTasks([...tasks, newTask]);
+  }
 
-    const removeTask = (taskId) => {
-        const updatedTasks = tasks.filter(task => task.id !== taskId);
-        setTasks(updatedTasks);
-    }
+  const removeTask = (taskId) => {
+      const updatedTasks = tasks.filter(task => task.id !== taskId);
+      setTasks(updatedTasks);
+  }
 
-    return (
-        <div className="App">
-            <h1>Get shit done</h1>
-            <ToDoForm addTask={addTask} />
-            <ToDoList tasks={tasks} removeTask={removeTask} />
-        </div>
-    );
+  const editTask = (taskId, newContent) => {
+      const updatedTasks = tasks.map(task => {
+          if (task.id === taskId) {
+              return { ...task, content: newContent };
+          }
+          return task;
+      });
+      setTasks(updatedTasks);
+  }
+
+  return (
+      <div className="App">
+          <h1>Get shit done</h1>
+          <ToDoForm addTask={addTask} />
+          <ToDoList tasks={tasks} removeTask={removeTask} editTask={editTask} />
+      </div>
+  );
 }
 
 export default App;
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
-const ToDoList = ({ tasks, removeTask }) => {
+function ToDoList({ tasks, removeTask, editTask }) {
+    const [editingTasks, setEditingTasks] = useState({});
+    const [editedContents, setEditedContents] = useState({});
+    
+    const handleEdit = (taskId) => {
+        setEditingTasks(prevState => ({
+            ...prevState,
+            [taskId]: true,
+        }));
+    }
+
+    const handleSave = (taskId) => {
+        const newContent = editedContents[taskId];
+        if (newContent !== undefined && newContent.trim() !== '') {
+            editTask(taskId, newContent);
+            setEditingTasks(prevState => ({
+                ...prevState,
+                [taskId]: false,
+            }));
+        }
+    }
+
+    const handleInputChange = (taskId, newValue) => {
+        setEditedContents(prevState => ({
+            ...prevState,
+            [taskId]: newValue,
+        }));
+    }
+
     return (
-        <div className="toDoList">
-            {tasks.map((task) => (
-                <div className="displayTask" key={task.id}>
-                    <p>{task.content}</p>
-                    <i className="fa-regular fa-pen-to-square faEdit"></i>
-                    <i className="fa-regular fa-trash-can faTrash" onClick={() => removeTask(task.id)}></i>
-                </div>
+        <ul>
+            {tasks.map(task => (
+                <li className="displayTask" key={task.id}>
+                    <div>
+                        {editingTasks[task.id] ? (
+                            <div>
+                                <input
+                                    type="text"
+                                    defaultValue={task.content}
+                                    onChange={(e) => handleInputChange(task.id, e.target.value)}
+                                    onBlur={() => handleSave(task.id)}
+                                />
+                                <button onClick={() => handleSave(task.id)}>Save</button>
+                            </div>
+                        ) : (
+                            <div>
+                                <p>{task.content}</p>
+                                <i className="fa-regular fa-pen-to-square faEdit" onClick={() => handleEdit(task.id)}></i>
+                                <i className="fa-regular fa-trash-can faTrash" onClick={() => removeTask(task.id)}></i>
+                            </div>
+                        )}
+                    </div>
+                </li>
             ))}
-        </div>
-    )
+        </ul>
+    );
 }
 
 export default ToDoList;
+
+
+
+
+
+
